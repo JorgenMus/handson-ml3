bb6c228431a8ca4a4e29798bc053d4fbb8098c66
JorgenMus <jirka.student@email.cz>
2026-06-29 19:33:55 +0200

    Jorgen> added first example showing reading data and using 2 different models to predict

--- jorgen_code/ch01_linear_model.py
+++ jorgen_code/ch01_linear_model.py
@@ -1,29 +1,28 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
-
+# Download and prepare the data
 data_root = "https://github.com/ageron/data/raw/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
-
 X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
 
-lifesat.plot(
-    kind="scatter",
-    grid=True,
-    x="GDP per capita (USD)",
-    y="Life satisfaction"
-)
-
+# Visualize the data
+lifesat.plot(kind='scatter', grid=True,
+             x="GDP per capita (USD)", y="Life satisfaction")
 plt.axis([23_500, 62_500, 4, 9])
 plt.show()
 
-model = LinearRegression()
+# Select a linear model
+#model = LinearRegression()
+model = KNeighborsRegressor(n_neighbors=3)
+
+# Train the model
 model.fit(X, y)
 
-X_new = [[37_655.2]]
-prediction = model.predict(X_new)
-
-print("Predicted life satisfaction for Cyprus:")
-print(prediction)
+# Make a prediction for Cyprus
+X_new = [[37_655.2]]  # Cyprus' GDP per capita in 2020
+print(model.predict(X_new)) # output: [[6.30165767]]
